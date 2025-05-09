01a6d08e3b2b809e0ccb7832f6c9e951e8e851ee
Laxman500apps <laxman.500apps@gmail.com>
2025-05-09 19:15:40 +0530

    fixed input issue

--- src/components/Body/GptSearchBox.jsx
+++ src/components/Body/GptSearchBox.jsx
@@ -3,9 +3,9 @@ import React from "react";
 const GptSearchBox = () => {
   return (
     <div className="w-screen h-screen bg-black flex justify-center items-start pt-30">
-      <form className="text-center">
+      <form onSubmit={(e) => e.preventDefault()} className="text-center">
         <input
-          className="w-[600px] p-3  border-2 border-white placeholder:text-white text-center font-bold rounded-3xl"
+          className="w-[600px] p-3 text-white border-2 border-white placeholder:text-white text-center font-bold rounded-3xl"
           type="text"
           placeholder="What do you wish to watch today?"
         />
